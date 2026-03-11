442a194a135d9f209ddb092e5a0b5be2d3f69984
phillous <theophiluseniayewu@gmail.com>
2026-03-11 09:14:21 -0700

    Deploying DB to Seveller

---
+++ backend/src/server.js
@@ -0,0 +1,37 @@
+import express from "express";
+import path from "path";
+import { clerkMiddleware } from "@clerk/express";
+
+import { ENV } from "./config/env.js";
+import { connectDB } from "./config/db.js";
+
+
+const app = express();
+
+const __dirname = path.resolve();
+
+app.use(express.json());
+app.use(clerkMiddleware()); // adds auth object under the req => req.auth
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true })); // credentials: true allows the browser to send the cookies to the server with the request
+
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ message: "Success" });
+});
+
+// make our app ready for deployment
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../admin/dist")));
+
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../admin", "dist", "index.html"));
+  });
+}
+
+const startServer = async () => {
+  await connectDB();
+  app.listen(ENV.PORT, () => {
+    console.log("Server is up and running");
+  });
+};
+
+startServer();
